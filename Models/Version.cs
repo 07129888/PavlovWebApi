aba27b6f2fffed67fe7d1bc7a4eff8627fb0c1ce
Miro <07129888@mail.ru>
2020-12-20 06:05:16 +0700

    extended versioncontroller #2

--- Models/Version.cs
+++ Models/Version.cs
@@ -7,5 +7,8 @@ namespace PavlovWebApi.Models
 {
     public class Version
     {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
     }
 }
