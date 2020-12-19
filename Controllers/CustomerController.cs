95c423c8f614a05679e30cbbe8abb2bbaaefff5a
Miro <07129888@mail.ru>
2020-12-20 05:57:30 +0700

    first commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PavlovWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PavlovWebApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private static List<CustomerData> _memCache = new List<CustomerData>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CustomerData>> Get()
+        {
+            return Ok(_memCache);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CustomerData> Get(int id)
+        {
+            if (_memCache.Count <= id) return NotFound("Неизвестный клиент");
+
+            return Ok(_memCache[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CustomerData value)
+        {
+            var validationResult = value.Validate();
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+            _memCache.Add(value);
+            return Ok($"{value.ToString()} добавлено");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] CustomerData value)
+        {
+            if (_memCache.Count <= id) return NotFound("Неизвестный клиент");
+            var validationResult = value.Validate();
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+            var previousValue = _memCache[id];
+            _memCache[id] = value;
+            return Ok($"{previousValue.ToString()} обновлено до {value.ToString()}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (_memCache.Count <= id) return NotFound("Неизвестный клиент");
+            var valueToRemove = _memCache[id];
+            _memCache.RemoveAt(id);
+            return Ok($"{valueToRemove.ToString()} было удалено");
+        }
+    }
+}
---
+++ Models/CustomerData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PavlovWebApi.Models
+{
+    public class CustomerData
+    {
+        public string Name { get; set; } // Имя клиента
+        public string Surname { get; set; } // Фамилия
+        public bool Gender { get; set; } // Пол, 0 - Ж, 1 - М
+        public DateTime VisitDate { get; set; } // Дата и время визита
+        public string Service { get; set; } // Услуга
+        public int Cost { get; set; } // Стоимость
+        public string Phone { get; set; } // Телефонный номер
+        public BaseModelValidationResult Validate()
+        {
+            var validationResult = new BaseModelValidationResult();
+
+            if (string.IsNullOrWhiteSpace(Name)) validationResult.Append($"Имя не может быть пустым");
+            if (string.IsNullOrWhiteSpace(Surname)) validationResult.Append($"Фамилия не может быть пустой");
+            if((DateTime.Now-VisitDate).TotalDays > 180) validationResult.Append($"Слишком давняя дата (более 6 месяцев назад)");
+            if (!string.IsNullOrEmpty(Name) && !char.IsUpper(Name.FirstOrDefault())) validationResult.Append($"Имя {Name} должно начинаться с большой буквы");
+            if (!string.IsNullOrEmpty(Surname) && !char.IsUpper(Surname.FirstOrDefault())) validationResult.Append($"Фамилия {Surname} Должна начинаться с большой буквы");
+            if (string.IsNullOrWhiteSpace(Service)) validationResult.Append($"Информация об оказанной услуге не может быть пустой");
+            if (Phone.Length != 11) validationResult.Append($"Телефон {Phone} должен содержать 11 символов, например 79993334455");
+
+            return validationResult;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}, {Gender}, {Phone}. {VisitDate}: {Service}, {Cost}";
+        }
+
+    }
+}
+
