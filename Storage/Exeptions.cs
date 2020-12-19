741c29765b00abdee3b041e17d082324c517fbe7
Miro <07129888@mail.ru>
2020-12-20 06:20:53 +0700

    add IStorage, MemCache, edit CustomerController

---
+++ Storage/Exeptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PavlovWebApi.Storage
+{
+    [System.Serializable]
+    public class IncorrectCustomerDataException : System.Exception
+    {
+        public IncorrectCustomerDataException() { }
+        public IncorrectCustomerDataException(string message) : base(message) { }
+        public IncorrectCustomerDataException(string message, System.Exception inner) : base(message, inner) { }
+        protected IncorrectCustomerDataException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
