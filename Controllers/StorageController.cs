2bfd41bb6bb6ae570b03a4e23f969a9240552216
Miro <07129888@mail.ru>
2020-12-21 22:56:05 +0700

    Add StorageController

---
+++ Controllers/StorageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PavlovWebApi.Controllers
+{
+    public class StorageController
+    {
+    }
+}
